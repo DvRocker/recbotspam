e8a6b3de029972da7ec361a357144395f5adb909
DvRocker <42909244+DvRocker@users.noreply.github.com>
2019-02-21 11:28:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
-
-client.on("ready", () => {
-let channel =     client.channels.get("545186396009267211")
-setInterval(function() {
-channel.send(`spam spam sppam spamspam spam spam spam spam spam spamspam spam spam spam spam spam spam
-spam spam spam spam spam spam spamspam spam spam spam spam spam spamspam spam spam spam spam spam spamspam spam spam spam spam spam spam`);
-}, 30)
-})
-
-client.login(process.env.BOT_TOKEN);
